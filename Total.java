36281053b34982affe131a3b2992eb7c2170abb5
anshuman172 <anshuman172@gmail.com>
2016-04-05 18:30:44 +0530

    Added files via upload

---
+++ Total.java
@@ -0,0 +1,15 @@
+public class Total {
+	
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		float b = 11;
+		
+		int total = a+(int)b;
+		
+		
+		System.out.println("The Sum is :" + total);
+		
+	}
+}
